2609ea6579fb8f50725a8f1dfb199bd3378791e2
Tony.Reina <greina@eng.ucsd.edu>
2018-09-10 14:28:55 -0700

    Use other model definition

--- src/baseline_train_classifier.py
+++ src/baseline_train_classifier.py
@@ -1,40 +1,11 @@
 import tensorflow as tf
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+import keras as K
 import numpy as np
 import os
-
-def focal_loss(gamma=2., alpha=.25):
-	def focal_loss_fixed(y_true, y_pred):
-		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-		return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
-	return focal_loss_fixed
-
 
 batch_size = 32
 image_shape = (1024,1024,1)
 epochs = 12
-
-
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-				 activation="relu",
-				 input_shape=image_shape))
-model.add(Conv2D(64, (3, 3), activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation="sigmoid"))
-
-model.compile(loss=[focal_loss(alpha=.25, gamma=2)],
-			  optimizer="Adam",
-			  metrics=["accuracy"])
 
 data_path = "../../rsna_data_numpy/"
 
@@ -48,9 +19,77 @@ labels_train = np.load(os.path.join(data_path, "labels_train.npy"),
 labels_test = np.load(os.path.join(data_path, "labels_test.npy"),
 						 mmap_mode="r", allow_pickle=False)
 
-tb_callback = keras.callbacks.TensorBoard(log_dir="./logs")
-model_callback = keras.callbacks.ModelCheckpoint("../models/baseline_classifier.h5", monitor="val_loss", verbose=1, save_best_only=True)
-early_callback = keras.callbacks.EarlyStopping(monitor="val_loss", patience=3, verbose=1)
+def focal_loss(y_true, y_pred, gamma=2., alpha=.25):
+
+	pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+	pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+	return -K.backend.sum(alpha * K.backend.pow(1. - pt_1, gamma)
+			* K.backend.log(pt_1))-K.backend.sum((1-alpha)
+			* K.backend.pow( pt_0, gamma) * K.backend.log(1. - pt_0))
+
+def dice_coef(y_true, y_pred, smooth=1.0):
+   intersection = tf.reduce_sum(y_true * y_pred)
+   union = tf.reduce_sum(y_true + y_pred)
+   numerator = tf.constant(2.) * intersection + smooth
+   denominator = union + smooth
+   coef = numerator / denominator
+   return tf.reduce_mean(coef)
+
+def define_model(input_shape=(1024,1024,1), dropout=0.5):
+
+	inputs = K.layers.Input(shape=input_shape, name="Images")
+
+	params = dict(kernel_size=(3, 3), activation="relu",
+				  padding="valid",
+				  kernel_initializer="he_uniform")
+
+	conv1 = K.layers.Conv2D(name="conv1a", filters=32, **params)(inputs)
+	conv1 = K.layers.Conv2D(name="conv1b", filters=32, **params)(conv1)
+	pool1 = K.layers.MaxPooling2D(name="pool1", pool_size=(2, 2))(conv1)
+
+	conv2 = K.layers.Conv2D(name="conv2a", filters=64, **params)(pool1)
+	conv2 = K.layers.Conv2D(name="conv2b", filters=64, **params)(conv2)
+	pool2 = K.layers.MaxPooling2D(name="pool2", pool_size=(2, 2))(conv2)
+
+	conv3 = K.layers.Conv2D(name="conv3a", filters=128, **params)(pool2)
+	# Trying dropout layers earlier on, as indicated in the paper
+	conv3 = K.layers.Dropout(dropout)(conv3)
+	conv3 = K.layers.Conv2D(name="conv3b", filters=128, **params)(conv3)
+
+	pool3 = K.layers.MaxPooling2D(name="pool3", pool_size=(2, 2))(conv3)
+
+	conv4 = K.layers.Conv2D(name="conv4a", filters=256, **params)(pool3)
+	# Trying dropout layers earlier on, as indicated in the paper
+	conv4 = K.layers.Dropout(dropout)(conv4)
+	conv4 = K.layers.Conv2D(name="conv4b", filters=256, **params)(conv4)
+
+	pool4 = K.layers.MaxPooling2D(name="pool4", pool_size=(2, 2))(conv4)
+
+	conv5 = K.layers.Conv2D(name="conv5a", filters=512, **params)(pool4)
+	conv5 = K.layers.Conv2D(name="conv5b", filters=512, **params)(conv5)
+
+
+	flat1 = K.layers.Flatten()(conv5)
+	dense1 = K.layers.Dense(128, activation="relu")(flat1)
+	drop1 = K.layers.Dropout(dropout)(dense1)
+	prediction = K.layers.Dense(1, activation="sigmoid")(drop1)
+
+	model = K.models.Model(inputs=[inputs], outputs=[prediction])
+
+	model.compile(loss=[focal_loss],
+				  optimizer="Adam",
+				  metrics=["accuracy", dice_coef])
+
+	return model
+
+
+tb_callback = K.callbacks.TensorBoard(log_dir="./logs")
+model_callback = K.callbacks.ModelCheckpoint("../models/baseline_classifier.h5", monitor="val_loss", verbose=1, save_best_only=True)
+early_callback = K.callbacks.EarlyStopping(monitor="val_loss", patience=3, verbose=1)
+
+
+model = define_model(image_shape)
 
 model.fit(imgs_train, labels_train,
 		  epochs=epochs,
