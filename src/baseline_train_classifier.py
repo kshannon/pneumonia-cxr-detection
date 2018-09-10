462c0889e912e7d2b115a68fd790d78e152a7424
Tony.Reina <greina@eng.ucsd.edu>
2018-09-10 13:59:24 -0700

    Fix focal loss

--- src/baseline_train_classifier.py
+++ src/baseline_train_classifier.py
@@ -1,4 +1,4 @@
-
+import tensorflow as tf
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -10,8 +10,8 @@ import os
 def focal_loss(gamma=2., alpha=.25):
 	def focal_loss_fixed(y_true, y_pred):
 		pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
-        	pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-        	return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
+		pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+		return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0))
 	return focal_loss_fixed
 
 
@@ -22,8 +22,8 @@ epochs = 12
 
 model = Sequential()
 model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation="relu",
-                 input_shape=image_shape))
+				 activation="relu",
+				 input_shape=image_shape))
 model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
@@ -33,8 +33,8 @@ model.add(Dropout(0.5))
 model.add(Dense(1, activation="sigmoid"))
 
 model.compile(loss=[focal_loss(alpha=.25, gamma=2)],
-              optimizer="Adam",
-              metrics=["accuracy"])
+			  optimizer="Adam",
+			  metrics=["accuracy"])
 
 data_path = "../../rsna_data_numpy/"
 
@@ -53,7 +53,7 @@ model_callback = keras.callbacks.ModelCheckpoint("../models/baseline_classifier.
 early_callback = keras.callbacks.EarlyStopping(monitor="val_loss", patience=3, verbose=1)
 
 model.fit(imgs_train, labels_train,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(imgs_test, labels_test),
-          callbacks=[tb_callback, model_callback, early_callback])
+		  epochs=epochs,
+		  verbose=1,
+		  validation_data=(imgs_test, labels_test),
+		  callbacks=[tb_callback, model_callback, early_callback])
