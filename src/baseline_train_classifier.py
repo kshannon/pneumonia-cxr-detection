55c1511b85a462d2eb724963a968b26ad5fa728d
Tony.Reina <greina@eng.ucsd.edu>
2018-09-10 12:00:08 -0700

    Adding simple model to do classification (LeNet). Adding converter from DICOM to numpy

---
+++ src/baseline_train_classifier.py
@@ -0,0 +1,47 @@
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import numpy as np
+import os
+
+
+batch_size = 32
+image_shape = (1024,1024,1)
+epochs = 12
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation="relu",
+                 input_shape=image_shape))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation="sigmoid"))
+
+model.compile(loss="binary_crossentropy",
+              optimizer="Adam",
+              metrics=["accuracy"])
+
+data_path = "rsna_data_numpy/"
+
+imgs_train = np.load(os.path.join(data_path, "imgs_train.npy"),
+						 mmap_mode="r", allow_pickle=False)
+imgs_test = np.load(os.path.join(data_path, "imgs_test.npy"),
+						 mmap_mode="r", allow_pickle=False)
+
+labels_train = np.load(os.path.join(data_path, "labels_train.npy"),
+						 mmap_mode="r", allow_pickle=False)
+labels_test = np.load(os.path.join(data_path, "labels_test.npy"),
+						 mmap_mode="r", allow_pickle=False)
+
+model.fit(imgs_train, labels_train,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(imgs_test, labels_test))
---
+++ src/utils/convert_data_to_numpy_files.py
@@ -0,0 +1,81 @@
+import numpy as np
+import SimpleITK as sitk
+import pandas as pd
+from tqdm import tqdm
+import os
+import matplotlib.pyplot as plt
+
+path = "rsna_data_numpy/"
+os.mkdir( path );
+
+df = pd.read_csv("stage_1_train_labels.csv")
+
+#filename = "training_images/6a35c51e-1d5b-4a08-b863-071916d08d79.dcm"
+#filename = "training_images/00704310-78a8-4b38-8475-49f4573b2dbb.dcm"
+filename = "training_images/00436515-870c-4b36-a041-de91049b9ab4.dcm"
+itkimage = sitk.ReadImage(filename)
+
+array = sitk.GetArrayFromImage(itkimage)
+array = np.fliplr(np.rot90(np.swapaxes(array,0,2),-1))
+
+# Do a train/test split (85%)
+# Note, this will cause some data leakage since multiple
+# lines in the CSV refer to the same DICOM file (i.e. > 1 bbox)
+msk = np.random.rand(df.shape[0])
+len_train = len(np.where(msk < 0.85)[0])
+len_test = len(np.where(msk >= 0.85)[0])
+
+imgs_train = np.zeros([len_train, array.shape[0], array.shape[1],1])
+labels_train = np.zeros(len_train)
+bboxes_train = np.zeros([len_train, 4])
+
+imgs_test = np.zeros([len_test, array.shape[0], array.shape[1],1])
+labels_test = np.zeros(len_test)
+bboxes_test = np.zeros([len_test, 4])
+
+print("Processing DICOM files and saving to Numpy format")
+
+idx_train = 0
+idx_test = 0
+idx = 0
+
+with tqdm(total=df.shape[0]) as pbar:
+    for index, row in df.iterrows():
+
+        filename = "training_images/" + row.patientId + ".dcm"
+
+        itkimage = sitk.ReadImage(filename)
+
+        array = sitk.GetArrayFromImage(itkimage)
+        array = np.fliplr(np.rot90(np.swapaxes(array,0,2),-1))
+        #plt.imshow(array[:,:,0], cmap="bone")
+        #plt.show()
+
+        if msk[idx] < 0.85:  # Train
+            imgs_train[idx_train, :, :, :] = array
+            labels_train[idx_train] = row.Target
+            bboxes_train[idx_train,0] = row.x
+            bboxes_train[idx_train,1] = row.y
+            bboxes_train[idx_train,2] = row.width
+            bboxes_train[idx_train,3] = row.height
+            idx_train += 1
+        else:
+            imgs_test[idx_test, :, :, :] = array
+            labels_test[idx_test] = row.Target
+            bboxes_test[idx_test,0] = row.x
+            bboxes_test[idx_test,1] = row.y
+            bboxes_test[idx_test,2] = row.width
+            bboxes_test[idx_test,3] = row.height
+            idx_test += 1
+
+        idx += 1
+
+        pbar.update(1)
+
+
+np.save(os.path.join(path, "imgs_train.npy"), imgs_train)
+np.save(os.path.join(path, "labels_train.npy"), labels_train)
+np.save(os.path.join(path, "bboxes_train.npy"), bboxes_train)
+np.save(os.path.join(path, "imgs_test.npy"), imgs_test)
+np.save(os.path.join(path, "labels_test.npy"), labels_test)
+np.save(os.path.join(path, "bboxes_test.npy"), bboxes_test)
